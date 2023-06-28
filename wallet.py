0fef5367c003e08127e5c48f23c12cf8cb22d8ff
Reut Farkash <reutfarkash@gmail.com>
2023-06-28 11:59:26 +0300

    wallet func names and close position

--- wallet.py
+++ wallet.py
@@ -3,31 +3,45 @@ from stock import Stock
 
 
 class Wallet(object):
-    def __init__(self, amount=0, options_sold=None, options_bought=None, stocks=None):
+    def __init__(
+        self, amount=0, call_options_sell=None, call_options_buy=None, stocks=None
+    ):
         self.amount = amount
-        self.options_sold = [] if options_sold is None else options_sold
-        self.options_bought = [] if options_bought is None else options_bought
+        self.call_options_sell = (
+            [] if call_options_sell is None else call_options_sell
+        )  # Short
+        self.call_options_buy = (
+            [] if call_options_buy is None else call_options_buy  # Long
+        )
         self.stocks = [] if stocks is None else stocks
 
-    def buy_option(self, option):
-        self.options_bought.append(option)
+    def buy_call_option(self, option):
+        self.call_options_buy.append(option)
         self.amount -= option.option_price
 
-    def sell_option(self, option):
-        self.options_sold.append(option)
+    def sell_call_option(self, option):
+        self.call_options_sell.append(option)
         self.amount += option.option_price
 
-    def realize_sold_option(self, ind=0):
-        self.amount += self.options_sold[ind].strike_price
-        del self.options_sold[ind]
+    def realize_sell_call_option(self, ind=0):
+        self.amount += self.call_options_sell[ind].strike_price
+        del self.call_options_sell[ind]
 
-    def realize_bought_option(self, ind=0):
-        self.amount -= self.options_bought[ind].strike_price
-        self.stocks.append(Stock.from_option(self.options_bought[ind]))
-        del self.options_bought[ind]
+    def realize_buy_call_option(self, ind=0):
+        self.amount -= self.call_options_buy[ind].strike_price
+        self.stocks.append(Stock.from_option(self.call_options_buy[ind]))
+        del self.call_options_buy[ind]
+
+    def close_sell_call_option(self, price, ind=0):
+        self.amount -= price
+        del self.call_options_sell[ind]
+
+    def close_buy_call_option(self, price, ind=0):
+        self.amount += price
+        del self.call_options_buy[ind]
 
     def __str__(self):
         return f"amount: {self.amount} \n\
-    options_bought: {' | '.join(str(itm) for itm in self.options_bought)} \n\
-    options_sold: {' | '.join(str(itm) for itm in self.options_sold)} \n\
-    stocks: {' | '.join(str(itm) for itm in self.stocks)}"
+    call_options_buy: <{' | '.join(str(itm) for itm in self.call_options_buy)}> \n\
+    call_options_sell: <{' | '.join(str(itm) for itm in self.call_options_sell)}> \n\
+    stocks: <{' | '.join(str(itm) for itm in self.stocks)}>"
