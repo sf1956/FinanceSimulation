f3034d902bb9ecb17cb24e8f64f96cc4bd04e1b3
Reut Farkash <reutfarkash@gmail.com>
2023-06-29 09:27:20 +0300

    clean up

--- poor_mans_covered_call.py
+++ poor_mans_covered_call.py
@@ -8,48 +8,49 @@ import matplotlib.pyplot as plt
 FUNCTIONS Section
 """
 
+
 def extract_rows_with_columns_equal_to_a_certain_value(df, column_name, value):
+    """
+    Creates a dataframe of rows with columns equal to a certain value in another.
 
-  """
-  Creates a dataframe of rows with columns equal to a certain value in another.
+    Args:
+      df: The dataframe to be filtered.
+      column_name: The name of the column to be checked for value.
+      value: The value to be filtered for.
 
-  Args:
-    df: The dataframe to be filtered.
-    column_name: The name of the column to be checked for value.
-    value: The value to be filtered for.
+    Returns:
+      A dataframe of the rows in df where the column_name column is equal to value.
+    """
 
-  Returns:
-    A dataframe of the rows in df where the column_name column is equal to value.
-  """
+    filtered_df = df[df[column_name] == value]
+    return filtered_df
 
-  filtered_df = df[df[column_name] == value]
-  return filtered_df
 
 ##########################################################
 
+
 def return_min_lt_A_in_col_X(df, A, X):
-    
-  """
+    """
 
-This function calculates the smallest value which is larger than A in column X of a DataFrame.
+    This function calculates the smallest value which is larger than A in column X of a DataFrame.
 
-Args:
-    df (pandas.DataFrame): The original DataFrame.
-    A (int): The value of A.
-    X (str): The name of the column X.
+    Args:
+        df (pandas.DataFrame): The original DataFrame.
+        A (int): The value of A.
+        X (str): The name of the column X.
 
-Returns:
-    float: The smallest value.
-  """
+    Returns:
+        float: The smallest value.
+    """
 
     # Calculate the smallest value in column X that is larger than A.
 
-  sm_lt_A = df[X].where(df[X] > A).min()
+    sm_lt_A = df[X].where(df[X] > A).min()
 
+    # Return the value.
 
-# Return the value.
+    return sm_lt_A
 
-  return sm_lt_A
 
 ###################################################
 
@@ -65,8 +66,8 @@ Returns:
     pandas.DataFrame: The filtered DataFrame.
 """
 
-def create_df_with_smalles_value_larger_then_A_of_column_X(df, A, X):
 
+def create_df_with_smalles_value_larger_then_A_of_column_X(df, A, X):
     # Calculate the smallest value in column X that is larger than A.
 
     smallest_values = df[X].where(df[X] > A).min()
@@ -77,11 +78,11 @@ def create_df_with_smalles_value_larger_then_A_of_column_X(df, A, X):
     # Return the filtered DataFrame.
     return filtered_df
 
+
 ############################################################
 
 
 def calculate_date(date_str, days):
-
     # Convert the date string to a datetime object.
     date = dt.datetime.strptime(date_str, "%Y-%m-%d")
 
@@ -91,12 +92,14 @@ def calculate_date(date_str, days):
     # Return the new date in the same format as the original date string.
     return new_date.strftime("%Y-%m-%d")
 
+
 ##################################
 
-def save_dataframe_to_csv(df, filename):
 
+def save_dataframe_to_csv(df, filename):
     # Save the DataFrame to a CSV file.
     df.to_csv(filename, index=False)
+
 
 #######################################################
 
@@ -104,23 +107,61 @@ def save_dataframe_to_csv(df, filename):
 
 # read data to DF
 
-df = pd.read_csv('./data/2015/qqq_eod_201501.txt')
+df = pd.read_csv("./data/2015/qqq_eod_201501.txt")
 
-df.rename(columns={'[QUOTE_UNIXTIME]':'QUOTE_UNIXTIME', ' [QUOTE_READTIME]':'QUOTE_READTIME', ' [QUOTE_DATE]':'QUOTE_DATE',
-       ' [QUOTE_TIME_HOURS]':'QUOTE_TIME_HOURS', ' [UNDERLYING_LAST]':'UNDERLYING_LAST', ' [EXPIRE_DATE]':'EXPIRE_DATE',
-       ' [EXPIRE_UNIX]':'EXPIRE_UNIX', ' [DTE]':'DTE', ' [C_DELTA]':'C_DELTA', ' [C_GAMMA]':'C_GAMMA', ' [C_VEGA]':'C_VEGA',
-       ' [C_THETA]':'C_THETA', ' [C_RHO]':'C_RHO', ' [C_IV]':'C_IV', ' [C_VOLUME]':'C_VOLUME', ' [C_LAST]':'C_LAST',
-       ' [C_SIZE]':'C_SIZE', ' [C_BID]':'C_BID', ' [C_ASK]':'C_ASK', ' [STRIKE]':'STRIKE', ' [P_BID]':'P_BID',
-       ' [P_ASK]':'P_ASK', ' [P_SIZE]':'P_SIZE', ' [P_LAST]':'P_LAST', ' [P_DELTA]':'P_DELTA', ' [P_GAMMA]':'P_GAMMA',
-       ' [P_VEGA]':'P_VEGA', ' [P_THETA]':'P_VEGA', ' [P_RHO]':'P_RHO', ' [P_IV]':'P_IV', ' [P_VOLUME]':'P_VOLUME',
-       ' [STRIKE_DISTANCE]':'STRIKE_DISTANCE', ' [STRIKE_DISTANCE_PCT]':'STRIKE_DISTANCE_PCT'}, inplace=True)
+df.rename(
+    columns={
+        "[QUOTE_UNIXTIME]": "QUOTE_UNIXTIME",
+        " [QUOTE_READTIME]": "QUOTE_READTIME",
+        " [QUOTE_DATE]": "QUOTE_DATE",
+        " [QUOTE_TIME_HOURS]": "QUOTE_TIME_HOURS",
+        " [UNDERLYING_LAST]": "UNDERLYING_LAST",
+        " [EXPIRE_DATE]": "EXPIRE_DATE",
+        " [EXPIRE_UNIX]": "EXPIRE_UNIX",
+        " [DTE]": "DTE",
+        " [C_DELTA]": "C_DELTA",
+        " [C_GAMMA]": "C_GAMMA",
+        " [C_VEGA]": "C_VEGA",
+        " [C_THETA]": "C_THETA",
+        " [C_RHO]": "C_RHO",
+        " [C_IV]": "C_IV",
+        " [C_VOLUME]": "C_VOLUME",
+        " [C_LAST]": "C_LAST",
+        " [C_SIZE]": "C_SIZE",
+        " [C_BID]": "C_BID",
+        " [C_ASK]": "C_ASK",
+        " [STRIKE]": "STRIKE",
+        " [P_BID]": "P_BID",
+        " [P_ASK]": "P_ASK",
+        " [P_SIZE]": "P_SIZE",
+        " [P_LAST]": "P_LAST",
+        " [P_DELTA]": "P_DELTA",
+        " [P_GAMMA]": "P_GAMMA",
+        " [P_VEGA]": "P_VEGA",
+        " [P_THETA]": "P_VEGA",
+        " [P_RHO]": "P_RHO",
+        " [P_IV]": "P_IV",
+        " [P_VOLUME]": "P_VOLUME",
+        " [STRIKE_DISTANCE]": "STRIKE_DISTANCE",
+        " [STRIKE_DISTANCE_PCT]": "STRIKE_DISTANCE_PCT",
+    },
+    inplace=True,
+)
 
 # d_df is a decimated DF with the required columns
 
-d_df = df[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'DTE', 'STRIKE', 'C_ASK']]
+d_df = df[["QUOTE_DATE", "UNDERLYING_LAST", "EXPIRE_DATE", "DTE", "STRIKE", "C_ASK"]]
+
 
 class Option(object):
-      def __init__(self, symbol='qqq', strike_price=100, stock_price=100, option_price=10, expiration_date=dt.date.fromisoformat('2023-12-04')):
+    def __init__(
+        self,
+        symbol="qqq",
+        strike_price=100,
+        stock_price=100,
+        option_price=10,
+        expiration_date=dt.date.fromisoformat("2023-12-04"),
+    ):
         self.symbol = symbol
         self.strike_price = strike_price
         self.stock_price = stock_price
@@ -137,8 +178,8 @@ d_df.EXPIRE_DATE = d_df.EXPIRE_DATE.str.strip()
 
 # change to rolling date !!!!
 
-trade_date = d_df.loc[0, 'QUOTE_DATE']
-initial_stock_price = d_df.loc[0, 'UNDERLYING_LAST']
+trade_date = d_df.loc[0, "QUOTE_DATE"]
+initial_stock_price = d_df.loc[0, "UNDERLYING_LAST"]
 
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 """ 
@@ -150,27 +191,33 @@ Sell (Write) Call Option Section
 
 # A is the upper limit of the desired number of days to expiration_date
 
-A=30
+A = 30
 
-DTE = return_min_lt_A_in_col_X(d_df, A, 'DTE')
+DTE = return_min_lt_A_in_col_X(d_df, A, "DTE")
 
-expiration_date = calculate_date(trade_date, DTE-3)     #check why this is correct!!!!
+expiration_date = calculate_date(trade_date, DTE - 3)  # check why this is correct!!!!
 
 # extract df with the calculated expiration_date
 
-df_expiration_date = extract_rows_with_columns_equal_to_a_certain_value(d_df, 'EXPIRE_DATE', expiration_date)
+df_expiration_date = extract_rows_with_columns_equal_to_a_certain_value(
+    d_df, "EXPIRE_DATE", expiration_date
+)
 
-#calculate Strike price
+# calculate Strike price
 
 Target_strike_stock_price_PCT = 0.05
 target_strike_price = initial_stock_price * (1 + Target_strike_stock_price_PCT)
-strike_price = return_min_lt_A_in_col_X(df_expiration_date, target_strike_price, 'STRIKE')
+strike_price = return_min_lt_A_in_col_X(
+    df_expiration_date, target_strike_price, "STRIKE"
+)
 
 # Create df of rows with strike_price in 'STRIKE' column
 
-df_strike = extract_rows_with_columns_equal_to_a_certain_value(df_expiration_date, 'STRIKE', strike_price)
+df_strike = extract_rows_with_columns_equal_to_a_certain_value(
+    df_expiration_date, "STRIKE", strike_price
+)
 
-#reset the indec of df_strike
+# reset the indec of df_strike
 
 df_strike.reset_index(drop=True, inplace=True)
 
@@ -203,27 +250,33 @@ Buy Call Option Section
 
 # A is the upper limit of the desired number of days to expiration_date
 
-A=700
+A = 700
 
-DTE = return_min_lt_A_in_col_X(d_df, A, 'DTE')
+DTE = return_min_lt_A_in_col_X(d_df, A, "DTE")
 
-expiration_date = calculate_date(trade_date, DTE+28)     #check why this is correct!!!!
+expiration_date = calculate_date(trade_date, DTE + 28)  # check why this is correct!!!!
 
 # extract df with the calculated expiration_date
 
-df_expiration_date = extract_rows_with_columns_equal_to_a_certain_value(d_df, 'EXPIRE_DATE', expiration_date)
+df_expiration_date = extract_rows_with_columns_equal_to_a_certain_value(
+    d_df, "EXPIRE_DATE", expiration_date
+)
 
-#calculate Strike price
+# calculate Strike price
 
 Target_strike_stock_price_PCT = 0
 target_strike_price = initial_stock_price * (1 + Target_strike_stock_price_PCT)
-strike_price = return_min_lt_A_in_col_X(df_expiration_date, target_strike_price, 'STRIKE')
+strike_price = return_min_lt_A_in_col_X(
+    df_expiration_date, target_strike_price, "STRIKE"
+)
 
 # Create df of rows with strike_price in 'STRIKE' column
 
-df_strike = extract_rows_with_columns_equal_to_a_certain_value(df_expiration_date, 'STRIKE', strike_price)
+df_strike = extract_rows_with_columns_equal_to_a_certain_value(
+    df_expiration_date, "STRIKE", strike_price
+)
 
-#reset the indec of df_strike
+# reset the indec of df_strike
 
 df_strike.reset_index(drop=True, inplace=True)
 
@@ -231,13 +284,10 @@ df_strike.reset_index(drop=True, inplace=True)
 # plot option & stock price
 
 fig, axs = plt.subplots(2, 1, sharex=True)
-axs[0].plot(df_strike['C_BID'])
-axs[0].set_title('Buy Call Option Price')
+axs[0].plot(df_strike["C_BID"])
+axs[0].set_title("Buy Call Option Price")
 axs[0].grid(True)
-axs[1].plot(df_strike['UNDERLYING_LAST'])
-axs[1].set_title('Stock Price')
+axs[1].plot(df_strike["UNDERLYING_LAST"])
+axs[1].set_title("Stock Price")
 axs[1].grid(True)
 plt.show()
-
-
-
